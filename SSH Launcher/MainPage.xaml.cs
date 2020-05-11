68280330a04ac0d38866dfbbf28da1ec2348e533
No GUI <colin@evaryont.me>
2020-05-10 23:53:33 -0700

    here's a bunch of code that mostly works
    doesn't actually handle the UnauthorizedAccessException properly, though. That'll need to be fixed.

--- SSH Launcher/MainPage.xaml.cs
+++ SSH Launcher/MainPage.xaml.cs
@@ -5,9 +5,12 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
 using Windows.Storage;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,44 +28,94 @@ namespace SSH_Launcher
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        const string SShHostPattern = @"(Host\s[\w\*]+)(\n\s+\w+\s.*)+";
-
         public MainPage()
         {
-            Console.WriteLine("New MainPage");
+
+            System.Diagnostics.Debug.WriteLine("new mainpage");
             this.InitializeComponent();
 
-            this.SshList.Items.Add("hello world");
-            this.debugLine.Text = "debug output goes here";
-            Console.WriteLine("Getting SSH hosts");
-            //this.GetSshHosts();
-            Console.WriteLine("..got em!");
+            List<string> ssh_hosts = new List<string>();
+            try
+            {
+                ssh_hosts = this.GetSshHosts();
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle((ex) =>
+                {
+                    if (ex is UnauthorizedAccessException)
+                    {
+                        var task = Task.Run(async () => {
+                            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                            {
+                                DisplayNoAccessDialog();
+                            });
+                        });
+                        task.Wait();
+                        Application.Current.Exit();
+                        return true;
+                    }
+                    return false;
+                });
+            }
+
+            foreach (string host in ssh_hosts)
+            {
+                this.SshList.Items.Add(host);
+            }
         }
 
         public List<String> GetSshHosts()
         {
-            var task = GetSshConfigContentsAsync();
-            //task.Wait();
+            var task = Task.Run(async () => {
+                return await GetSshConfigContentsAsync();
+            });
             string ssh_config_text = task.Result;
             List<string> ssh_hosts = new List<string>();
 
-            //foreach (Match m in Regex.Matches(ssh_config_text, SShHostPattern))
-            //{
-            //    Console.WriteLine("new ssh host found: " + m.Value);
-            //}
+            foreach (string line in ssh_config_text.Split(new char[] { '\r', '\n' }))
+            {
+                if (line.StartsWith("host", StringComparison.OrdinalIgnoreCase))
+                {
+                    List<string> hosts_in_line = line.Trim().Split(' ').ToList();
+                    hosts_in_line.RemoveAt(0); // Delete the first item in the list, "host"
+                    foreach (string host in hosts_in_line)
+                    {
+                        System.Diagnostics.Debug.WriteLine("new ssh host found: " + host);
+                        ssh_hosts.Add(host);
+                    }
+                }
+            }
 
-            Console.WriteLine(ssh_config_text);
-
-            return ssh_hosts;
+            return ssh_hosts.Distinct().ToList();
         }
 
-        public async Task<string> GetSshConfigContentsAsync()
+        private async Task<string> GetSshConfigContentsAsync()
         {
             string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
             string ssh_config_path = Path.Combine(home, ".ssh", "config");
             StorageFile ssh_config = await StorageFile.GetFileFromPathAsync(ssh_config_path);
             string text = await FileIO.ReadTextAsync(ssh_config);
             return text;
+        }
+
+        private async void DisplayNoAccessDialog()
+        {
+            ContentDialog noAccessDialog = new ContentDialog
+            {
+                Title = "No file access",
+                Content = "This application requires file access to function. Please allow it access under Settings > Privacy > File Access.",
+                CloseButtonText = "Ok"
+            };
+
+            // Use this code to associate the dialog to the appropriate AppWindow by setting
+            // the dialog's XamlRoot to the same XamlRoot as an element that is already present in the AppWindow.
+            if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 8))
+            {
+                noAccessDialog.XamlRoot = this.SshList.XamlRoot;
+            }
+
+            ContentDialogResult result = await noAccessDialog.ShowAsync();
         }
     }
 }
---
+++ SshConfigParser/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Environment;
+
+namespace SshConfigParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ssh_config_text = GetSshContents();
+
+            foreach (string line in ssh_config_text.Split(new char[] { '\r', '\n' }))
+            {
+                //Console.WriteLine(line);
+                if (line.StartsWith("host", StringComparison.OrdinalIgnoreCase))
+                {
+                    List<string> hosts_in_line = line.Trim().Split(' ').ToList();
+                    hosts_in_line.RemoveAt(0); // Delete the first item in the list, "host"
+                    foreach (string host in hosts_in_line)
+                    {
+                        Console.WriteLine("new ssh host found: " + host);
+                    }
+                }
+            }
+        }
+
+        static public string GetSshContents()
+        {
+            var home = Environment.GetFolderPath(SpecialFolder.UserProfile);
+            var text = File.ReadAllText(Path.Join(home, ".ssh", "config"));
+            return text;
+        }
+    }
+}
